9c5c76b0822f571df184fdac8d79f6da08e94f27
Alexis <alexis.mendez@anima.edu.uy>
2024-06-04 09:29:01 -0300

    XD

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+export {Productos} from "Producto.js"
+
+export const App =() =>{
+
+  <Productable Producto ={Productos}/>
+  
+}
+export function Productable ({Productos}){
+  return <li>{Productos}</li>
+
+  
+}
+export function Categoryfile ({}){
+
+}
+export function Productfile ({}){
+  
+}
